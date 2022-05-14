9c2b0ff5787b2b8073fcdf4266b96dca6d7b49a8
sivaramakr <sivaramakrishna2884@gmail.com>
2022-05-14 08:27:26 +0530

    master

---
+++ photoapp/src/main/java/com/photoapp/photoapp/controller/UserController.java
@@ -0,0 +1,17 @@
+//package com.photoapp.photoapp.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/users")
+//public class UserController {
+//	
+//	@GetMapping(path="/check")
+//	public String status() {
+//		
+//		return "vhecked";
+//		}
+//
+//}
